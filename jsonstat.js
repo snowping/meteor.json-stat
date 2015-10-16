6d87b2e2a3eece3b11579d603bbb0ca93fe47903
Joël Poyet <joel.poyet@ecologic.ch>
2015-10-16 16:13:01 -0500

    first commit

---
+++ jsonstat.js
@@ -0,0 +1,1 @@
+JSONstat = Npm.require('jsonstat');
---
+++ package.js
@@ -0,0 +1,20 @@
+var JSONstatVersion = '0.8.3';
+
+Package.describe({
+  name: 'snowping:json-stat',
+  version: JSONstatVersion + '_1',
+  summary: 'Wrapper for npm module json-stat',
+  git: '',
+  documentation: 'README.md'
+});
+
+//NPM module dependencies
+Npm.depends({
+   'jsonstat': JSONstatVersion
+});
+
+Package.onUse(function(api) {
+  api.export('JSONstat', 'server');
+  api.addFiles('jsonstat.js', 'server')
+});
+
